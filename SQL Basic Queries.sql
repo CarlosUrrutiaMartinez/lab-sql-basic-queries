68a98c4d68e0658c2d2dcd8cae1e21f2667cc9de
FreeKingOfCats <carlosurrutia91@gmail.com>
2025-01-18 13:52:31 +0100

    Fix

--- SQL Basic Queries.sql
+++ SQL Basic Queries.sql
@@ -1,16 +1,23 @@
 USE sakila;
 
+USE sakila;
+SELECT table_name
+FROM information_schema.tables
+WHERE table_schema = 'sakila';
+
 SELECT * FROM actor;
 SELECT * FROM film;
 SELECT * FROM customer;
 
+SELECT * FROM rental;
+
 
 SELECT title FROM film;
 
-SELECT * FROM sakila.language;
+SELECT * FROM language;
 SELECT f.title, l.name AS "language"
 FROM film AS f
-INNER JOIN sakila.language AS l
+INNER JOIN language AS l
 ON f.language_id = l.language_id;
 
 SELECT first_name
@@ -21,20 +28,33 @@ SELECT DISTINCT release_year
 FROM film;
 
 SELECT * FROM inventory;
+SELECT * FROM film;
 SELECT * FROM store;
 SELECT * FROM rental;
+SELECT * FROM address;
+SELECT * FROM staff;
 
 SELECT COUNT(*) AS num_stores
 FROM store;
+
+SELECT COUNT(DISTINCT address_id) AS num_store_addresses
+FROM address;
+
 SELECT COUNT(*) AS num_employees
 FROM staff;
+
 SELECT 
   (SELECT COUNT(*) FROM inventory) AS films_available,
   (SELECT COUNT(*) FROM rental) AS films_rented;
+
+SELECT COUNT(*) FROM inventory AS films_available;
+
+SELECT COUNT(*) FROM rental AS films_rented;
+  
 SELECT COUNT(DISTINCT last_name) AS num_distinct_last_names
 FROM actor;
 
-SELECT film_id, title, length
+SELECT title, length
 FROM film
 ORDER BY length DESC
 LIMIT 10;
